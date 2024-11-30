5283bdf4e9da73d109ba6e8395b9e3f30f37bee6
Talraen <talraen@gmail.com>
2024-11-30 00:30:02 -0500

    Added basic scenario output and user data saving

--- modules/scenarios.js
+++ modules/scenarios.js
@@ -1,9 +1,10 @@
-import { readSpoiler, loadJSON } from './utility.js';
+import { readSpoiler, makeToggle, loadJSON } from './utility.js';
 
 // Handle scenario data loaded from JSON
 class Scenarios {
     static loadPromise;
     static loadedData;
+    static userData;
 
     // Load data from JSON (force reload if previously load is complete)
     static loadData = function() {
@@ -26,12 +27,101 @@ class Scenarios {
             return Scenarios.loadData();
         }
     }
+
+    // Gets the current state of user data
+    static getUserData = function() {
+        if (!Scenarios.userData) {
+            try {
+                Scenarios.userData = JSON.parse(window.localStorage.getItem('user-scenario-data'));
+            } catch (error) {
+                Scenarios.userData = null;
+            }
+            if (Scenarios.userData === null) {
+                // Default (scenarios 0 and 1 revealed)
+                Scenarios.userData = {
+                    0: {
+                        status: 'revealed',
+                    },
+                    1: {
+                        status: 'revealed',
+                    },
+                }
+            }
+        }
+        return Scenarios.userData;
+    }
+
+    // Saves current user data to local storage
+    static updateUserData = function() {
+        window.localStorage.setItem('user-scenario-data', JSON.stringify(Scenarios.userData));
+    }
 }
 
+// Full scenario list rewrite based on current data
 function updateScenarios() {
+    const $scenarioList = document.getElementById('scenario-list');
     Scenarios.getData().then(scenarios => {
+        const userData = Scenarios.getUserData();
         for (const [number, scenario] of Object.entries(scenarios)) {
-            console.log(readSpoiler(scenario.name));
+            if (!userData[number]) {
+                continue;
+            }
+            const userScenario = userData[number];
+
+            const $li = document.createElement('li');
+
+            const $number = document.createElement('span');
+            $number.classList.add('scenario-number');
+            $number.appendChild(document.createTextNode(readSpoiler(scenario.number)));
+            $li.appendChild($number);
+
+            const $name = document.createElement('span');
+            $name.classList.add('scenario-name');
+            $name.appendChild(document.createTextNode(readSpoiler(scenario.name)));
+            $li.appendChild($name);
+
+            const $status = document.createElement('div');
+            $status.classList.add('scenario-status-toggle');
+            makeToggle($status, [
+                {
+                    text: 'Revealed',
+                    class: 'revealed',
+                },
+                {
+                    text: 'Complete',
+                    class: 'complete',
+                },
+                {
+                    text: 'Locked Out',
+                    class: 'locked',
+                },
+            ], status => {
+                userScenario.status = status;
+                Scenarios.updateUserData();
+            }, {current: userScenario.status});
+            $li.appendChild($status);
+
+            if (scenario.randomItem) {
+                const $randomItem = document.createElement('div');
+                $randomItem.classList.add('scenario-random-item-toggle');
+                $randomItem.classList.add(userScenario.randomItem || 'not-found');
+                makeToggle($randomItem, [
+                    {
+                        text: 'Random Item Not Found',
+                        class: 'not-found',
+                    },
+                    {
+                        text: 'Random Item Found',
+                        class: 'found',
+                    },
+                ], status => {
+                    userScenario.randomItem = status === 'found';
+                    Scenarios.updateUserData();
+                });
+                $li.appendChild($randomItem);
+            }
+
+            $scenarioList.appendChild($li);
         }
     });
 }
--- modules/utility.js
+++ modules/utility.js
@@ -13,7 +13,7 @@ const popConfirm = message =>
             resolve();
         } else {
             reject();
-        }    
+        }
     });
 
 const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
@@ -84,6 +84,13 @@ const readSpoilerObject = data => {
     return spoiled;
 };
 
+// Quickly clear out DOM element
+function emptyElement($element) {
+    while ($element.firstChild) {
+        $element.removeChild($element.firstChild);
+    }
+}
+
 function makeRadioButtons($buttons, clickCallback, { deselect = false } = {}) {
     [...$buttons].forEach($button => {
         // Give created radio buttons the radio-button class
@@ -116,6 +123,45 @@ function makeRadioButtons($buttons, clickCallback, { deselect = false } = {}) {
     });
 }
 
+function makeToggle($element, options, clickCallback, { current = null } = {}) {
+    $element.classList.add('toggle');
+
+    let defaultOption = options[0];
+    if (current !== null) {
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].class === current) {
+                defaultOption = options[i];
+                break;
+            }
+        }
+    }
+    assignToggleOption($element, defaultOption);
+
+    $element.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        for (let i = 0; i < options.length; i++) {
+            if ($element.classList.contains(options[i].class)) {
+                $element.classList.remove(options[i].class);
+                clickCallback(assignToggleOption($element, options[(i + 1) % options.length]));
+                break;
+            }
+        }
+    });
+}
+
+// Sets an option on a toggle, returns the current class
+function assignToggleOption($element, option) {
+    emptyElement($element);
+    $element.appendChild(document.createTextNode(option.text));
+    if (option.class) {
+        $element.classList.add(option.class);
+        return option.class;
+    } else {
+        return false;
+    }
+}
+
 function copyText(text, $textarea) {
     $textarea.value = text;
     if (typeof $textarea.select === 'function') {
@@ -143,4 +189,4 @@ function readTabs(tabText, { headers = true } = {}) {
     return data;
 }
 
-export { popMessage, popError, popConfirm, capitalize, hash, hideSpoiler, readSpoiler, makeRadioButtons, loadJSON, copyText, readTabs };
+export { popMessage, popError, popConfirm, capitalize, hash, hideSpoiler, readSpoiler, emptyElement, makeRadioButtons, makeToggle, loadJSON, copyText, readTabs };
